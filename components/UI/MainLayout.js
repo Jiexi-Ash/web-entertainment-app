82326c5fad0593f026fd805096de70474f9ea684
Jiexi-Ash <lance.jiexi@gmail.com>
2022-10-06 13:24:16 +0200

    added Head component to MainLayout and added favicon

--- components/UI/MainLayout.js
+++ components/UI/MainLayout.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 import RouteGuard from "helpers/RouteGuard";
 import SideNav from "components/Navbar/SideNav/SideNav";
 import Navbar from "components/Navbar/Navbar";
@@ -7,6 +8,11 @@ import { ToastContainer } from "react-toastify";
 function MainLayout({ children }) {
   return (
     <RouteGuard>
+      <Head>
+        <title>Web Entertainment App</title>
+        <meta name="description" content="Netflix Clone" />
+        <link rel="icon" href="/assets/favicon-32x32.png" />
+      </Head>
       <div className="relative min-h-screen lg:flex ">
         <SideNav />
         <main className="flex-1  lg:ml-36 lg:mt-16 overflow-auto">
